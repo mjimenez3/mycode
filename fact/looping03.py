4eca7af146a90c10f5851c71be3cbbff5cde4349
Michael Jimenez <mjimenez3@liberty.edu>
2020-09-23 16:39:19 +0000

    looping tech updates

--- fact/looping03.py
+++ fact/looping03.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 #this library allows us to generate UUID values.
 import uuid
-
+from  time import sleep
 howmany = int(input("How many UUIDs should be generated? "))
 
-print("Generating UUIDs...")
+print("Generating UUIDs..."); sleep(0.8)
 
 # range is required because an int cannot be looped
 for rando in range(howmany):
