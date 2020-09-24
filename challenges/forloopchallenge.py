070c235df83f765cae8b6702f06168ce664145c1
Michael Jimenez <mjimenez3@liberty.edu>
2020-09-24 16:10:13 +0000

    Forloopchallenge

---
+++ challenges/forloopchallenge.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+
+def one():
+    
+    star= ['*','**','***','****','*****','****','***','**','*']
+    for p in star:
+        print(p)
+
+
+def two():
+    num = input("how many rows do you want: ")
+    for p in range(1, num+1):
+        print("* "*a)
+        
+    for b in range(num-1,0,-1):
+        print("* "*b)
+two()
+
+
+
