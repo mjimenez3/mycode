5ace04f5bfdd2ced16aa94cc8ec53925587bf349
Michael Jimenez <mjimenez3@liberty.edu>
2020-09-25 14:24:49 +0000

    more dice oop

---
+++ oop2/cheatdice.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from random import randint
+
+class Player:
+  def __init__(self):
+    self.dice = []
+
+  def roll(self):
+    self.dice = [] # clears current dice
+    for i in range(3):
+      self.dice.append(randint(1,6))
+
+  def get_dice(self):
+    return self.dice #shares dice
+
+class Cheat_Swapper(Player): # new class
+  def cheat(self):  
+    self.dice[-1] = 6  # last die is = 6
+
+class Cheat_Loaded_Dice(Player):
+  def cheat(self):
+    i = 0
+    while i < len(self.dice): # i is less than length of list (3)
+      if self.dice[i] < 6:  # if it's less than 6
+        self.dice[i] += 1 #then add 1
+      i += 1  # add 1 to i, and loop again.
+
---
+++ oop2/dice_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from cheatdice import Player
+from cheatdice import Cheat_Swapper
+from cheatdice import Cheat_Loaded_Dice
+
+# two different cheaters
+cheater1 = Cheat_Swapper()
+cheater2 = Cheat_Loaded_Dice()
+
+#both roll normal
+cheater1.roll()
+cheater2.roll()
+
+#both to their cheat
+cheater1.cheat()
+cheater2.cheat()
+
+# sharing scores
+print("Cheater 1 rolled" + str(cheater1.get_dice()))
+print("Cheater 2 rolled" + str(cheater2.get_dice()))
+
+if sum(cheater1.get_dice()) == sum(cheater2.get_dice()):
+  print("Draw!")
+
+elif sum(cheater1.get_dice()) > sum(cheater2.get_dice()):
+  print("Cheater 1 wins!")
+
+else:
+  print("Cheater 2 wins!")
+
